a2826a32901b66d141aa8766f75415671d43f351
Hakkology <yildizhakan88@gmail.com>
2024-03-03 23:28:17 +0300

    Search Invoice page, debouncing.

--- Notes/libraryReminders.cpp
+++ Notes/libraryReminders.cpp
@@ -18,6 +18,19 @@ Specific client behaviour, needs to be researched thoroughly.
 import { unstable_noStore as noStore } from 'next/cache';
 For disabling cache behaviour
 
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+useSearchParams- Allows you to access the parameters of the current URL. 
+usePathname - Lets you read the current URL's pathname. 
+useRouter - Enables navigation between routes within client components programmatically.
+
+defaultValue vs. value / Controlled vs. Uncontrolled
+If you're using state to manage the value of an input, you'd use the value attribute to make it a controlled component. 
+This means React would manage the input's state.
+However, since you're not using state, you can use defaultValue. 
+This means the native input will manage its own state. This is okay since you're saving the search query to the URL instead of state.
+
+import { useDebouncedCallback } from 'use-debounce';
+limits the rate at which a function can fire.
 
 
 */
